677865d9f3c591ef32f5c00cd49c872774d7d9c2
Skrypit <fordemt2@gmail.com>
2022-09-05 01:10:52 +0300

    Fixed problem with  adding styles from index.css

--- src/main/java/com/groupone/security/WebSecurityConfig.java
+++ src/main/java/com/groupone/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.groupone.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -13,5 +14,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable();
+
+//        http.authorizeRequests().antMatchers("static/**").permitAll();
     }
+
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web
+//                .ignoring()
+//                .antMatchers("static/**");
+//    }
 }
